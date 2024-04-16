27c18663c0cfb4a95f785f630df3fd791ddbfd47
Steval91 <167100080+Steval91@users.noreply.github.com>
2024-04-16 23:40:34 +0700

    Remove field name from post response dto.

--- src/main/java/dev/steve/blogrestapi/dto/post/PostResponse.java
+++ src/main/java/dev/steve/blogrestapi/dto/post/PostResponse.java
@@ -1,42 +1,39 @@
 package dev.steve.blogrestapi.dto.post;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostResponse {
-    @Schema(description = "Post's ID")
-    private Long id;
 
-    @Schema(description = "Post's Name")
-    private String name;
+  @Schema(description = "Post's ID")
+  private Long id;
 
-    @Schema(description = "Post's Title")
-    private String title;
+  @Schema(description = "Post's Title")
+  private String title;
 
-    @Schema(description = "Post's Body")
-    private String body;
+  @Schema(description = "Post's Body")
+  private String body;
 
-    @Schema(description = "Post's Author")
-    private String author;
+  @Schema(description = "Post's Author")
+  private String author;
 
-    @Schema(description = "Created By")
-    private Long createdBy;
+  @Schema(description = "Created By")
+  private Long createdBy;
 
-    @Schema(description = "Updated By")
-    private Long updatedBy;
+  @Schema(description = "Updated By")
+  private Long updatedBy;
 
-    @Schema(description = "Created Date")
-    private Date createdAt;
+  @Schema(description = "Created Date")
+  private Date createdAt;
 
-    @Schema(description = "Updated Date")
-    private Date updatedAt;
+  @Schema(description = "Updated Date")
+  private Date updatedAt;
 }
